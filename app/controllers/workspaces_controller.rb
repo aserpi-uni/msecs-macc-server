9d293618da5ce8c2cc8a76263d1d3c20ea6c88b9
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-13 16:11:11 +0100

    Add workspace creation

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -13,6 +13,7 @@ class WorkspacesController < ApplicationController
 
   # GET /workspaces/new
   def new
+    @workspace = Workspace.new
   end
 
   # GET /workspaces/1/edit
@@ -22,6 +23,23 @@ class WorkspacesController < ApplicationController
   # POST /workspaces
   # POST /workspaces.json
   def create
+    params = workspace_params.permit(%i[billable description name])
+    params[:admin] = current_admin
+    @workspace = Workspace.from_params(params)
+
+    respond_to do |format|
+      if @workspace.save
+        format.html do
+          redirect_to @workspace, flash: { success: I18n.t(:new_success,
+                                                           scope: :resource,
+                                                           resource: Workspace.model_name.human.capitalize) }
+        end
+        format.json { render :show, status: :created, location: @workspace }
+      else
+        format.html { render :new }
+        format.json { render json: @workspace.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /workspaces/1
@@ -33,11 +51,16 @@ class WorkspacesController < ApplicationController
   # DELETE /workspaces/1.json
   def destroy
   end
-  
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_workspace
     @workspace = Workspace.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def workspace_params
+    params.require(:workspace)
   end
 end
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,3 +1,15 @@
 class Workspace < ApplicationRecord
   belongs_to :admin
+
+  # Create a new Workspace from +create+ action parameters.
+  def self.from_params(params)
+    workspace = Workspace.new
+
+    workspace.admin = params[:admin]
+    workspace.billable = params[:billable]
+    workspace.description = params[:description]
+    workspace.name = params[:name]
+
+    workspace
+  end
 end
