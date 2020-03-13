9fe04ce87545784842f6054dfbf069b61ad5b1b3
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-13 18:13:07 +0100

    Use import syntax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,6 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-require("jquery");
-require("popper.js");
-require("bootstrap");
-require("daemonite-material");
-require("typeface-roboto");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,3 +15,9 @@ require("typeface-roboto");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "jquery"
+import "popper.js"
+import "bootstrap"
+import "daemonite-material"
+import "typeface-roboto"
