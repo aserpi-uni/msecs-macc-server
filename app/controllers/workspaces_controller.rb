d9984dcc1316427457462a49e9b3f571fedea44e
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-13 15:08:29 +0100

    Add workspace controller and views

---
+++ app/controllers/workspaces_controller.rb
@@ -0,0 +1,34 @@
+class WorkspacesController < ApplicationController
+  # GET /workspaces
+  # GET /workspaces.json
+  def index
+  end
+
+  # GET /workspaces/1
+  # GET /workspaces/1.json
+  def show
+  end
+
+  # GET /workspaces/new
+  def new
+  end
+
+  # GET /workspaces/1/edit
+  def edit
+  end
+
+  # POST /workspaces
+  # POST /workspaces.json
+  def create
+  end
+
+  # PATCH/PUT /workspaces/1
+  # PATCH/PUT /workspaces/1.json
+  def update
+  end
+
+  # DELETE /workspaces/1
+  # DELETE /workspaces/1.json
+  def destroy
+  end
+  end
