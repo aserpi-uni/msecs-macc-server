418ccaa0b98a0cee42c6e53f58c1319dd04c439f
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-17 16:18:40 +0100

    Monetise worker

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,11 +3,16 @@ class Worker < ApplicationRecord
 
   has_and_belongs_to_many :workspaces
 
+  monetize :bill_rate_cents, with_model_currency: :currency
+
   def self.from_params(params)
     worker = Worker.new
 
     worker.email = params[:email]
     worker.password = Devise.friendly_token 32
+
+    worker.bill_rate_cents = 0
+    worker.currency = MoneyRails.default_currency.iso_code
 
     worker
   end
