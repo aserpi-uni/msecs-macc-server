6a731f98e68a11abffde4d492e09dc7769b66fd9
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-04-01 20:00:07 +0200

    Add client model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_165003) do
+ActiveRecord::Schema.define(version: 2020_04_01_152849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_03_19_165003) do
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.bigint "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_clients_on_name", unique: true
   end
 
   create_table "worker_authentication_tokens", force: :cascade do |t|
