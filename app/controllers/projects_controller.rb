2b6326bc55a2b91a77c08d766525eac15b6333e3
Carlo Alberto Caracciolo <caracciolo.1550367@studenti.uniroma1.it>
2020-08-29 20:12:18 +0200

    added editing and deleting on projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,6 +18,19 @@ class ProjectsController < ApplicationController
     redirect_to @project
 
   end
+  def edit
+    @project = Project.find(params[:id])
+  end
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to @project
+  end
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
 
   private
   def project_params
