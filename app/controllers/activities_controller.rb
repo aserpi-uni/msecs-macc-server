60edea6f35cdb6526e05e75503dfd923bae7a3eb
Carlo Alberto Caracciolo <caracciolo.1550367@studenti.uniroma1.it>
2020-08-30 18:47:07 +0200

    started implementing activity inside project

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,15 +1,21 @@
 class ActivitiesController < ApplicationController
   def index
-
+    @activity= policy_scope(Activity)
   end
   def new
-
+    @activity = Activity.new
   end
   def create
-
+    params = project_params
+    params[:admin] = current_admin
+    params[:project]
+    @project = Project.from_params(params)
+    print(params[:status])
+    @project.save
+    redirect_to @project
   end
   def show
-
+    @activity = Activity.find(params[:id])
   end
   def edit
 
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,9 @@ class Activity < ApplicationRecord
   belongs_to :project, polymorphic: true
   def self.from_params(params)
     activity = Activity.new
-
+    activity.admin = params[:admin]
+    activity.project = params[:project]
+    activity.description = params[:description]
+    activity
   end
 end
