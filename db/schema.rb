2fbabdf6a97a35f7af02ef3ad05354fdb5d8ecc5
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-09-04 00:54:55 +0200

    Remove unused attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_215701) do
+ActiveRecord::Schema.define(version: 2020_09_03_223751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
-    t.string "activity_description"
     t.datetime "delivery_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
